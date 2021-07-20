9a8d9db12ec8be1df322f2d883b7aa7978fceebc
Jayman874 <66066183+Jayman874@users.noreply.github.com>
2021-07-20 22:59:15 +1200

    Add files via upload

---
+++ src/Card.java
@@ -0,0 +1,10 @@
+
+public interface Card {
+	
+	public String getName();
+	
+	public boolean setName(String name);
+	
+	public String toString();
+
+}
---
+++ src/Guess.java
@@ -0,0 +1,25 @@
+
+public class Guess {
+	Character character;
+	Weapon weapon;
+	Estate estate;
+	Guess murderer;
+	
+	public Guess(Character c, Weapon w, Estate e) {
+		this.character = c;
+		this.weapon = w;
+		this.estate = e;
+	}
+	
+	public void setMurderer(Guess g) {
+		murderer = g;	
+	}
+	
+	public Guess getMurderer() {
+		return murderer;
+	}
+	
+	public String toString() {
+		return character.toString() + ", " + weapon.toString() + ", " + estate.toString();
+	}
+}
