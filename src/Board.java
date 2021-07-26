5b412e55fcb90b8bd16428d68f36f0a5cc266c1c
Louis Cooper <louistobiascooper@gmail.com>
2021-07-26 16:23:45 +1200

    board updates

--- src/Board.java
+++ src/Board.java
@@ -33,29 +33,29 @@ public class Board{
     Estate villaCelia = Game.getEstate("Villa Celia");
     Estate calamityCastle = Game.getEstate("Calamity Castle");
     Estate perilPalace = Game.getEstate("Peril Palace");
-      for(int i = 0; i < 5; i++) { // nested for loop to set an estate to all the cells that need it
-        for(int j = 0; j < 5; j++) {
-          Cell cell;
-          cell = cells[2 + i][2 + j];
-          cell.setEmpty(false);
-          cell.setEstate(hauntedHouse);
+    for(int i = 0; i < 5; i++) { // nested for loop to set an estate to all the cells that need it
+      for(int j = 0; j < 5; j++) {
+        Cell cell;
+        cell = cells[2 + i][2 + j];
+        cell.setEmpty(false);
+        cell.setEstate(hauntedHouse);
 
-          cell = cells[2 + i][17 + j];
-          cell.setEmpty(false);
-          cell.setEstate(manicManor);
+        cell = cells[2 + i][17 + j];
+        cell.setEmpty(false);
+        cell.setEstate(manicManor);
 
-          cell = cells[9 + i][10 + j];
-          cell.setEmpty(false);
-          cell.setEstate(villaCelia);
+        cell = cells[9 + i][10 + j];
+        cell.setEmpty(false);
+        cell.setEstate(villaCelia);
 
-          cell = cells[17 + i][2 + j];
-          cell.setEmpty(false);
-          cell.setEstate(calamityCastle);
+        cell = cells[17 + i][2 + j];
+        cell.setEmpty(false);
+        cell.setEstate(calamityCastle);
 
-          cell = cells[17 + i][17 + j];
-          cell.setEmpty(false);
-          cell.setEstate(perilPalace);
-        }
+        cell = cells[17 + i][17 + j];
+        cell.setEmpty(false);
+        cell.setEstate(perilPalace);
+      }
     }
   }
 
